6e889a80e7e2b6c5a813ae7ba9b4deaff6e4a13c
RUDRASONI1906 <rudrasoni1906@gmail.com>
2024-11-04 11:58:09 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.currencyconverter.cecproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.currencyconverter.cecproject to javafx.fxml;
+    exports com.example.currencyconverter.cecproject;
+}
